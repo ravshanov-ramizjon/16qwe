07535f14edb53cd43c0c000fba40e4aab7d661b0
ravshanov-ramizjon <ravsanovramiz@gmail.com>
2025-04-24 22:35:28 +0500

    фикс багов

---
+++ lib/authOptions.ts
@@ -0,0 +1,17 @@
+// lib/authOptions.ts
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+};
